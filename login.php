a70d83a1d264de1de1398acd96ca4ac4f4d8e615
Si-ID <noval27nov@gmail.com>
2025-05-15 07:31:25 +0700

    edit dikit

--- login.php
+++ login.php
@@ -26,14 +26,14 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Form Register</title>
+    <title>LOGIN</title>
     <link rel="stylesheet" href="CSS/form.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
   </head>
   <body>
     <div class="form">
         <form action="" method="post">
-            <h2>Login Form</h2>
+            <h2>Login Thrifture</h2>
             <p class="msg"></p>
 
             <div class="form-group">
