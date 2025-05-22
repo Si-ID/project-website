4a7a4c1f74a9ec55f37e01ae829fe3fa8cddc242
Si-ID <noval27nov@gmail.com>
2025-05-22 18:40:00 +0700

    menghosting web agar database online

--- connect.php
+++ connect.php
@@ -1,5 +1,10 @@
 <?php
-    $koneksi = mysqli_connect("localhost","root","","thrifture");
+
+    $host = "sql12.freesqldatabase.com";
+    $user = "sql12780537";
+    $pass = "AxPy15ZKVF";
+    $db   = "sql12780537";
+    $koneksi = mysqli_connect("$host","$user","$pass","$db");
 
     if (!$koneksi) {
         echo "Connect Gagal";
